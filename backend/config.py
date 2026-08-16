7e21dd45d394a63f9a3283ec2251a86595adebb1
mcsr14724 <chandumaddasani145@gmail.com>
2026-08-16 15:15:47 +0530

    added collecton and embedding size

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,5 @@ class Config:
     GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
     QDRANT_API_KEY=os.getenv("QDRANT_API_KEY")
     QDRANT_URL=os.getenv("QDRANT_URL")
+    QDRANT_COLLECTION=os.getenv("QDRANT_COLLECTION")
+    EMBEDDING_SIZE=os.getenv("EMBEDDING_SIZE")
