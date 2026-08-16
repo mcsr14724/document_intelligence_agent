d401561b6a9ba6e102c6780bac5d7924d40d6b49
mcsr14724 <chandumaddasani145@gmail.com>
2026-08-16 15:35:16 +0530

    added lifesapn which runs create_collection on startup

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from backend.database.qdrantdb import create_collection
 from backend.routes.documents import router as document_router
 from backend.routes.chat import router as chat_router
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    create_collection()
+    yield
 
 
 app = FastAPI(
     title="Document Intelligence Agent",
     description="RAG-based document search and intelligence API",
+    lifespan=lifespan
 )
 
 app.include_router(document_router)
